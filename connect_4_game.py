a27cd60e7cbf238dd9cb3e302cf166304c170612
Niya Mateeva (Nalan Bilgin) <119063181+Polishko@users.noreply.github.com>
2023-06-01 21:20:54 +0300

    Update connect_4_game.py
    Improved readability

--- connect_4_game.py
+++ connect_4_game.py
@@ -1,10 +1,11 @@
 from collections import deque
-ROWS = 6
-COLS = 7
+ROWS, COLS = 6, 7
 CONNECT_RANGE = 4
 
 
 def get_valid_column():
+    print(f"Player {player}, please choose a column:")
+
     while True:
         try:
             val = int(input())
@@ -16,59 +17,50 @@ def get_valid_column():
             print("Column out of range/ Enter new value: ")
             continue
 
-        if len(list(filter(lambda x: (x == 0), [game_matrix[r][val - 1] for r in range(ROWS)]))) == 0:
+        if len(list(filter(lambda x: (x == 0), [board[r][val - 1] for r in range(ROWS)]))) == 0:
             print("Slot full/Choose another slot: ")
             continue
         else:
             return val - 1
 
 
-def check_4_connection(r, c):
-    rows_range = (max(r - CONNECT_RANGE, 0), min(r + CONNECT_RANGE, ROWS))
-    cols_range = (max(c - CONNECT_RANGE, 0), min(c + CONNECT_RANGE, COLS))
+def set_slot(a, b):
+    for row in range(ROWS - 1, -1, -1):
+        if board[row][b] == 0:
+            board[row][b] = a
+            [print(" ".join([str(x) for x in board[row]])) for row in range(ROWS)]
+            return [row, b]
 
-    second_diagonal = [game_matrix[a][b] for a in range(rows_range[0], rows_range[1])
-                       for b in range(cols_range[0], cols_range[1]) if a + b == r + c]
 
-    first_diagonal = [game_matrix[a][b] for a in range(rows_range[0], rows_range[1])
-                      for b in range(cols_range[0], cols_range[1]) if a - b == r - c]
+def check_for_connection(r, c):
+    global loop
+    low_r, high_r = max(r - CONNECT_RANGE, 0), min(r + CONNECT_RANGE, ROWS)
+    low_c, high_c = max(c - CONNECT_RANGE, 0), min(c + CONNECT_RANGE, COLS)
 
-    vertical = [game_matrix[a][c] for a in range(rows_range[0], rows_range[1])]
-
-    horizontal = [game_matrix[r][b] for b in range(cols_range[0], cols_range[1])]
+    second_diagonal = [board[a][b] for a in range(low_r, high_r) for b in range(low_c, high_c) if a + b == r + c]
+    first_diagonal = [board[a][b] for a in range(low_r, high_r) for b in range(low_c, high_c) if a - b == r - c]
+    vertical = [board[a][c] for a in range(low_r, high_r)]
+    horizontal = [board[r][b] for b in range(low_c, high_c)]
 
     lists = deque([second_diagonal] + [first_diagonal] + [vertical] + [horizontal])
 
     while lists:
         current_line = "".join(str(x) for x in lists.popleft())
         if CONNECT_RANGE * str(player) in current_line:
-            return True
+            print(f"Player {player} won")
+            loop = False
+            break
 
-    return False
 
-
-game_matrix = [[0 for c in range(COLS)] for r in range(ROWS)]
+board = [[0 for c in range(COLS)] for r in range(ROWS)]
 [print(" ".join(str(0) for col in range(COLS))) for row in range(ROWS)]
 
 players = deque([1, 2])
 
-while True:
+loop = True
+while loop:
     player = players.popleft()
-    player_point = [0, 0]
-    print(f"Player {player}, please choose a column:")
     column = get_valid_column()
     players.append(player)
-
-    for row in range(ROWS - 1, -1, -1):
-        slot = game_matrix[row][column]
-
-        if slot == 0:
-            game_matrix[row][column] = player
-            player_point = [row, column]
-            break
-
-    [print(" ".join([str(x) for x in game_matrix[row]])) for row in range(ROWS)]
-
-    if check_4_connection(player_point[0], player_point[1]):
-        print(f"Player {player} won")
-        break
+    player_point = set_slot(player, column)
+    check_for_connection(player_point[0], player_point[1])
