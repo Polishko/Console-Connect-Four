fd62bd49116c13b3c1a541aa5db8f01825998059
Niya Mateeva (Nalan Bilgin) <119063181+Polishko@users.noreply.github.com>
2023-06-01 22:03:38 +0300

    Update connect_4_game.py
    Added more functionality, draw condition and colors

--- connect_4_game.py
+++ connect_4_game.py
@@ -1,34 +1,40 @@
 from collections import deque
+from colorama import Fore
+
 ROWS, COLS = 6, 7
 CONNECT_RANGE = 4
 
 
 def get_valid_column():
-    print(f"Player {player}, please choose a column:")
+    print(Fore.BLUE + f"Player {player}, please choose a column:" + Fore.RESET)
 
     while True:
         try:
             val = int(input())
         except ValueError:
-            print("Enter a valid value:")
+            print(Fore.RED + f"Enter a valid value:" + Fore.RESET)
             continue
 
         if not 0 <= val - 1 < COLS:
-            print("Column out of range/ Enter new value: ")
+            print(Fore.RED + f"Column out of range/ Enter new value: " + Fore.RESET)
             continue
 
         if len(list(filter(lambda x: (x == 0), [board[r][val - 1] for r in range(ROWS)]))) == 0:
-            print("Slot full/Choose another slot: ")
+            print(Fore.RED + "Slot full/Choose another slot: " + Fore.RESET)
             continue
         else:
             return val - 1
+
+
+def print_board(a):
+    [print(*row, sep=" ") for row in board]
 
 
 def set_slot(a, b):
     for row in range(ROWS - 1, -1, -1):
         if board[row][b] == 0:
             board[row][b] = a
-            [print(" ".join([str(x) for x in board[row]])) for row in range(ROWS)]
+            print_board(board)
             return [row, b]
 
 
@@ -47,13 +53,20 @@ def check_for_connection(r, c):
     while lists:
         current_line = "".join(str(x) for x in lists.popleft())
         if CONNECT_RANGE * str(player) in current_line:
-            print(f"Player {player} won")
+            print(Fore.GREEN + f"Player {player} won" + Fore.RESET)
             loop = False
             break
 
 
+def check_if_draw(a):
+    global loop
+    if all([item != 0 for r in a for item in r]):
+        print(Fore.BLUE + f"It's a draw!" + Fore.RESET)
+        loop = False
+
+
 board = [[0 for c in range(COLS)] for r in range(ROWS)]
-[print(" ".join(str(0) for col in range(COLS))) for row in range(ROWS)]
+print_board(board)
 
 players = deque([1, 2])
 
@@ -64,3 +77,4 @@ while loop:
     players.append(player)
     player_point = set_slot(player, column)
     check_for_connection(player_point[0], player_point[1])
+    check_if_draw(board)
